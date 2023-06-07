c05af5903290587827ff1229e22f0e9de4f9a509
NhatNK-43 <khanh.nhat.365@gmail.com>
2023-06-08 00:04:41 +0700

    Nhat submit practice of ss7

---
+++ ss7_variable_datatypes_operator/practice/practice2/practice2.js
@@ -0,0 +1,4 @@
+let inputWidth = prompt('Enter the width');
+let inputHeight = prompt('Enter the height');
+let area = parseFloat(inputWidth)*parseFloat(inputHeight);
+document.write('The area is: '+area)
